c59fa96e684efacd019d93f74eb20a416a74909d
habib-prog <xavierjames701@gmail.com>
2026-01-15 19:58:51 +0600

    Chat msg front end developing

--- src/Components/Chats/Chat.jsx
+++ src/Components/Chats/Chat.jsx
@@ -16,9 +16,9 @@ const Chat = () => {
     <div className="chats">
       <div className="top">
         <div className="userinformation">
-          <img className="avatar" src="/public/avatar.png" />
+          <img className="avatar" src="/public/Kathy.jpeg" />
           <div className="text">
-            <span className="uname">Kaushik Raj</span>
+            <span className="uname">Carol Kathy</span>
             <p className="udes">Active now</p>
           </div>
         </div>
@@ -28,7 +28,112 @@ const Chat = () => {
           <img className="iconsize" src="/public/info.png" alt="info" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        {/* msg start 1 */}
+        <div className="msg">
+          <img className="iconsize" src="/public/avatar.png" alt="" />
+          <div className="texts">
+            <p className="content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+              obcaecati nostrum
+            </p>
+            <span className="time">sent 1 minuite ago</span>
+          </div>
+        </div>
+        {/* msg start 2 */}
+        <div className="msg own">
+          {/* <img className="iconsize" src="/public/avatar.png" alt="" /> */}
+          <div className="texts">
+            <p className="content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+              obcaecati nostrum
+            </p>
+            <span className="time">sent 1 minuite ago</span>
+          </div>
+        </div>
+        {/* Own msg 3 */}
+        <div className="msg ">
+          <img className="iconsize" src="/public/avatar.png" alt="" />
+          <div className="texts">
+            <p className="content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+              obcaecati nostrum
+            </p>
+            <span className="time">sent 1 minuite ago</span>
+          </div>
+        </div>
+        {/* Own msg 4 */}
+        <div className="msg own">
+          {/* <img className="iconsize" src="/public/avatar.png" alt="" /> */}
+          <div className="texts">
+            <p className="content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+              obcaecati nostrum
+            </p>
+            <span className="time">sent 1 minuite ago</span>
+          </div>
+        </div>
+        {/* Own msg 5 */}
+        <div className="msg ">
+          <img className="iconsize" src="/public/avatar.png" alt="" />
+          <div className="texts">
+            <p className="content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+              obcaecati nostrum
+            </p>
+            <span className="time">sent 10 minuite ago</span>
+          </div>
+        </div>
+        {/* Own msg 6 */}
+        <div className="msg own">
+          {/* <img className="iconsize" src="/public/avatar.png" alt="" /> */}
+          <div className="texts">
+            <p className="content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+              obcaecati nostrum
+            </p>
+            <span className="time">Seen 8 minuite ago</span>
+          </div>
+        </div>
+        {/* Own msg 7 */}
+        <div className="msg ">
+          <img className="iconsize" src="/public/avatar.png" alt="" />
+          <div className="texts">
+            <p className="content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+              obcaecati nostrum
+            </p>
+            <span className="time">sent 5 minuite ago</span>
+          </div>
+        </div>
+        {/* Own msg 8 */}
+        <div className="msg own">
+          {/* <img className="iconsize" src="/public/avatar.png" alt="" /> */}
+          <div className="texts">
+            <img src="/public/Kathy.jpeg" alt="" />
+            <p className="content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+              obcaecati nostrum
+            </p>
+            <span className="time">Seen 1 minuite ago</span>
+          </div>
+        </div>
+        {/* Own msg 9 */}
+        <div className="msg ">
+          <img className="iconsize" src="/public/avatar.png" alt="" />
+          <div className="texts">
+            <img
+              src="/public/WhatsApp Image 2025-12-31 at 14.33.03.jpeg"
+              alt=""
+            />
+            <p className="content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+              obcaecati nostrum
+            </p>
+            <span className="time">Sent 1 minuite ago</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img className="iconsize" src="/public/image-.png" alt="image" />
